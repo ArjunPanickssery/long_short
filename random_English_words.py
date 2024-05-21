a84fc582d4ccfe24f784168908e5e00829ef3050
Arjun Panickssery <arjun.panickssery@gmail.com>
2024-05-21 22:41:33 +0200

    Refactored data

--- random_English_words.py
+++ random_English_words.py
@@ -3,15 +3,16 @@ from tqdm import tqdm
 import json
 
 NUM_WORDS = 10000
-FILE_NAME = 'random_words.json'
+FILE_NAME = "random_words.json"
 
-r = RandomWords()
+if __name__ == "__main__":
+    r = RandomWords()
 
-words = []
-for _ in tqdm(range(NUM_WORDS)):
-    words.append(r.get_random_word())
+    words = []
+    for _ in tqdm(range(NUM_WORDS)):
+        words.append(r.get_random_word())
 
-with open(FILE_NAME, 'w') as f:
-    json.dump(words, f)
+    with open(FILE_NAME, "w") as f:
+        json.dump(words, f)
 
-print(f'Done! Saved to {FILE_NAME}')
+    print(f"Done! Saved to {FILE_NAME}")
